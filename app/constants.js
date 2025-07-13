28c3f02b9691e26d352a6670627aa41d9f36eaba
Ranjith <ranjithp2294@gmail.com>
2025-07-13 16:55:44 +0530

    constants are updated

--- app/constants.js
+++ app/constants.js
@@ -1,2 +1,4 @@
-export const CLIENT_A_SERVICE = "http://localhost:3000";
-export const CLIENT_B_SERVICE = "http://localhost:3001";
+// export const CLIENT_A_SERVICE = "http://localhost:3000";
+// export const CLIENT_B_SERVICE = "http://localhost:3001";
+export const CLIENT_A_SERVICE = "https://clienta.edemy.shop";
+export const CLIENT_B_SERVICE = "https://clientb.edemy.shop";
