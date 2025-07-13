0d3076a61301426ee1ea1839a20d2e83f295bff5
Ranjith <ranjithp2294@gmail.com>
2025-07-13 12:30:04 +0530

    landig page design improved

--- src/features/LandingPage.jsx
+++ src/features/LandingPage.jsx
@@ -1,10 +1,19 @@
 import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <button onClick={() => navigate("/clientA")}>Continue as client-A</button>
-      <button onClick={() => navigate("/clientB")}>Continue as client-B</button>
+    <div className="landing-page">
+      <h1 className="landing-title">Welcome</h1>
+      <div className="button-group">
+        <button onClick={() => navigate("/clientA")} className="landing-button">
+          Continue as Client-A
+        </button>
+        <button onClick={() => navigate("/clientB")} className="landing-button">
+          Continue as Client-B
+        </button>
+      </div>
     </div>
   );
 };
